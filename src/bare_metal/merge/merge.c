d3a7993a96475ad9c9604e621fa6f810461c7971
Simon Vinding Brodersen <simon@3450.dk>
2024-05-10 09:16:14 +0200

    change to merge

--- src/bare_metal/merge/merge.c
+++ src/bare_metal/merge/merge.c
@@ -3,14 +3,9 @@
 #include "../threads/threads.h"
 #include <stddef.h>
 #include <stdint.h>
-#include <string.h>
 int merge_sort(void *input_list, size_t size, size_t length) {}
 
 int parallel_merge_sort(void *input_list, size_t size, size_t length) {
-  if (queue_length() >= NUM_CORES) {
-    merge_sort(input_list, size, length);
-    return 0;
-  }
   thread_t left_thread, right_thread;
   uint32_t left_length = length / 2;
   uint32_t right_length = length - left_length;
