76b5d7489f4e65bba2fcb21d7492cad3e69428ac
Simon Vinding Brodersen <simon@3450.dk>
2024-05-28 11:22:13 +0200

    Added random_numbers check

--- src/bare_metal/random_numbers.py
+++ src/bare_metal/random_numbers.py
@@ -1,9 +1,20 @@
-import random
 import sys
+import random
+
 
 if __name__ == "__main__":
     lst = []
-    num = int(sys.argv[1])
-    for i in range(num):
-        lst.append(random.randint(0, num))
-    print(lst)
+    num_args = len(sys.argv)
+    if num_args != 4:
+        print("""USAGE: %d %d %d\n\t Where the first argument is lower range,
+              second is upper range and last is number of elements""")
+        exit(1)
+    l = int(sys.argv[1])
+    u = int(sys.argv[2])
+    num = int(sys.argv[3])
+
+    lst = [random.randint(l, u) for _ in range(num)]
+    with open("./alist.c", "w") as f:
+        f.write(
+            f"volatile int alist[{num}] = {{{str(lst)[1:-1]}}};\nvolatile int alist_size = {num};"
+        )
