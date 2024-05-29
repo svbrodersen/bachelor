c3b1335414a6684dd5397bdbe9b46e6e2e087c30
Simon Vinding Brodersen <simon@3450.dk>
2024-05-29 11:52:53 +0200

    Work on Evaluation with fixes to validate.py

--- src/bare_metal/validate.py
+++ src/bare_metal/validate.py
@@ -2,10 +2,20 @@ data = []
 with open("./test.txt", "r") as f:
     data = f.read().split("\n")
 
-lst1 = [int(x) for x in data[0].split(",") if x.isdigit()]
-lst2 = [int(x) for x in data[1].split(",") if x.isdigit()]
 
-assert len(lst1) == len(lst2)
+def hybrid_is_int(x):
+    try:
+        int(x)
+        return True
+    except ValueError:
+        return False
+
+
+lst1 = [int(x) for x in data[0].split(",") if hybrid_is_int(x)]
+lst2 = [int(x) for x in data[1].split(",") if hybrid_is_int(x)]
+
 lst1.sort()
+print(len(lst1))
+assert len(lst2) == len(lst1)
 for i in range(len(lst1)):
-    assert lst1[i] == lst2[i], f"{i}"
+    assert lst1[i] == lst2[i], f"{i}: {lst1[i]} != {lst2[i]}"
